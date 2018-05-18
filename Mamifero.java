697b5bfbabcaf01961f80a66a6a813cf09e1c542
Carlos Alvarez Gonzalez <LitosLpr@gmail.com>
2018-05-18 09:37:42 +0200

    Completada la granja con las clases adecuadas

---
+++ Mamifero.java
@@ -0,0 +1,22 @@
+public abstract class Mamifero extends Animal
+{
+    private static final int INCREMENTO_COMER = 2;
+    private int calidadRaza;
+
+    public Mamifero(int calidadRaza)
+    {
+        super();
+        this.calidadRaza = calidadRaza;
+    }
+
+    public void comer()
+    {
+        setPeso(getPeso() + INCREMENTO_COMER);
+        super.comer();
+        if (calidadRaza >= 5)
+        {
+            variaPuntosDeVida(calidadRaza);
+        }
+    }
+
+}
